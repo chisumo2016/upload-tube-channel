d3ad476b5849cb6568d04bcfa9c7afac01f4b8a6
= <=>
2017-08-24 15:16:56 +0100

    Created a channel model and relationship completed

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -26,4 +27,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public  function  channel()
+    {
+        return $this->hasMany(Channel::class);
+    }
 }
